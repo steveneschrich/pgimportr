6332d81b7a704d0f466d57b3363b1d703e4a4fa0
Steven Eschrich <35569991+steveneschrich@users.noreply.github.com>
2024-09-05 15:10:17 -0400

    Fixes #1 The definition of is_esi_related is expanded to former and current (either) status. Also implemented functions for former and current specifically.

--- R/is.R
+++ R/is.R
@@ -15,8 +15,70 @@
 #' @return
 #'
 #' @examples
-is_esi_related <- function(.x) {
-  purrr::map_lgl(.x, function(.y) {
-    any(.y$partnership_role == "ESI")
-  })
+#is_esi_related <- function(.x) {
+#  purrr::map_lgl(.x, function(.y) {
+#    any(.y$partnership_role == "ESI")
+#  })
+#}
+
+
+
+#' Determine if any people are Current ESI status
+#' @description This function provides a way, in mutate, to test if any
+#' of the authors are Current ESI status.
+#'
+#' @details It is a little tricky, since the authors are nested, to test
+#' if any of the authors are ESI's. This code is private to the library
+#' since the whole purpose of the function is to create an indicator
+#' variable so that we don't need to do this regularly.
+#'
+#' @param people A data frame of investigators or authors
+#'
+#' @return A logical indicating if any of the people are Current ESI
+#' @export
+#'
+#' @examples
+#' \dontrun{calculate_is_current_esi_related(investigators[[1]])}
+calculate_is_current_esi_related <- function(people) {
+  any(people[["isPartnershipRole_ESI"]])
 }
+#'  Determine if any people are Former ESI status
+#'
+#' @param people A data frame of investigators or authors
+#' @description This function provides a way, in mutate, to test if any
+#' of the authors are Former ESI status.
+#'
+#' @details It is a little tricky, since the authors are nested, to test
+#' if any of the authors are ESI's. This code is private to the library
+#' since the whole purpose of the function is to create an indicator
+#' variable so that we don't need to do this regularly.
+#'
+#' @return A logical indicating if any of the people are Former ESI
+#' @export
+#'
+#' @examples
+#' \dontrun{calculate_is_former_esi_related(investigators[[1]])}
+calculate_is_former_esi_related <- function(people) {
+  any(people[["isPartnershipRole_Former ESI"]])
+}
+#' Determine if any people are ESI status
+#' @description This function provides a way, in mutate, to test if any
+#' of the authors are ESI (Current or Former).
+#'
+#' @details It is a little tricky, since the authors are nested, to test
+#' if any of the authors are ESI's. This code is private to the library
+#' since the whole purpose of the function is to create an indicator
+#' variable so that we don't need to do this regularly.
+
+#' @param people
+#'
+#' @return A data frame of investigators or authors
+#' @export
+#'
+#' @examples
+#' \dontrun{calculate_is_esi_related(investigators[[1]])}
+calculate_is_esi_related <- function(people) {
+  calculate_is_current_esi_related(people) | calculate_is_former_esi_related(people)
+}
+
+
